2d2556595538805939bdf9a0cea3ca75fa4da287
leoblam <leo.belgacem@gmail.com>
2024-01-08 11:52:52 +0100

    initialisation of the project

---
+++ server.js
@@ -0,0 +1,13 @@
+// Chargement des variables d'environement
+// Import des modules necessaires
+// Import de routes pour l'authentification
+// Import de la configuraton de la base de donnees
+// Initialisation de l'application Express
+// Middleware pour traiter les requetes JSON
+// Middlware oiur parser lkes corps de requetes
+// Utilisation des routes pour l'authentification
+// Configuration des options cors
+// Middleware pour gerer les cors
+// Definition du port de demarrage du serveur
+// Fonction pour demarrer le serveur
+// Appel de la fonction pour demarrer le serveur
