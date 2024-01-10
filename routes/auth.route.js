b0b88b0fcb1d45388e61325de241ac30b75fcedd
leoblam <leo.belgacem@gmail.com>
2024-01-10 12:01:31 +0100

    feat seting up the routes ouf authMiddleware

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 
+const authMiddleware = require("../middleware/authentificate");
+
 // Route pour l'inscription
 router.post("/register", authController.register);
 
 // Route pour connection
 router.post("/login", authController.login);
 
-// router.post("/athentificate", authController.athentificate);
+// Route protegee
+router.get("/dashboard", authMiddleware.authentificate, (req, res) => {
+  // Verifier si l"utilisateur est un admin
+  if (req.user.role === admin) {
+    // Definition de req.isAdmin sera egal a true pour les administrateur
+    req.isAdmin = true;
+    // Envoyer une reponse de succes
+    return res.status(200).json({ message: "Bienvenu administrateur" });
+  } else {
+    //Envoyer une reponse pour les utilisateurs non admin
+    return res.status(403).json({
+      message:
+        "Action non autorisee, seul lesadministrateurs peuvent acceder a cette page",
+    });
+  }
+});
 
 module.exports = router;
